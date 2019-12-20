92bb7d4ff595c78763d1975812cd59f6b9b87117
justinelai <justine.lai@gmail.com>
2019-12-20 11:34:18 -0500

    pseudocode notes

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,18 +3,30 @@ from doubly_linked_list import DoublyLinkedList
 
 class RingBuffer:
     def __init__(self, capacity):
-        self.capacity = capacity
-        self.current = None
+        self.capacity = capacity # integer, fixed
+        self.current = None # Index of oldest element... location where "current"/new element will be placed?
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # adds elements to the buffer - no APPEND method. reassignment?
+        # oldest element in the ring buffer is overwritten with the newest element
+        
+        self.current = 0 #initialize empty DLL.
+        
+        if self.storage.length == self.capacity: #if DLL is full
+            pass
+        self.storage.add_to_tail(item)
+        self.current += 1 # current must increase, but should be able to loop back to zero if it reaches capacity.
+
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        # returns all of the elements in the buffer in a list in their given order (OLDEST VALUE FIRST? if so, not necessarily head or tail)
+        # do not return None values in the list even if they are present
+        for entry in range(self.storage.length):
+           list_buffer_contents.append(entry)
 
         return list_buffer_contents
 
