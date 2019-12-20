dd8d923047e3f398ac37a8353bb5e251b4e8bd60
justinelai <justine.lai@gmail.com>
2019-12-20 13:22:49 -0500

    names done

--- names/names.py
+++ names/names.py
@@ -1,5 +1,4 @@
 import time
-from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -10,29 +9,39 @@ f.close()
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
-
-"""duplicates = []
+"""
+duplicates = []
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
-            duplicates.append(name_1)"""
+            duplicates.append(name_1)
+"""
 
-bst_1 = BinarySearchTree("")
-for name_1 in names_1: 
-    bst_1.insert(name_1)
+names_1.sort()
+names_2.sort()
 
-bst_2 = BinarySearchTree("")
-for name_2 in names_2: 
-    bst_2.insert(name_2)
+duplicates = []
 
+j = 0
+k = 0
+
+while j <= 9999 and k <= 9999:
+    if names_1[j] == names_2[k]:
+        duplicates.append(names_1[j])
+        j += 1
+        k += 1
+    elif names_1[j] < names_2[k]:
+        j += 1
+    else:
+        k += 1
+    
 
 
 # ------- Student Response ----------
-# Runtime on my machine was actually 9.307297706604004 seconds! Ouch.
+# Runtime on my machine was actually around 9-10 seconds! Ouch.
 # The runtime complexity is O(n^2).
-# 
-
-
+# I reduced it to 0.021649837493896484 seconds.
+# 64 duplicates.
 # -----------------------------------
 
 end_time = time.time()
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,19 +4,20 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity # integer, fixed
-        self.current = None # Index of oldest element... location where "current"/new element will be placed?
+        self.current = None # Quasi index of oldest element... location where "current"/new element will be placed? but only for a linked list.
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        # adds elements to the buffer - no APPEND method. reassignment?
+        # adds elements to the buffer - no APPEND method. reassignment? FIFO
         # oldest element in the ring buffer is overwritten with the newest element
-        
-        self.current = 0 #initialize empty DLL.
-        
+        # instead, oldest element is head
+        self.current = self.storage.head
         if self.storage.length == self.capacity: #if DLL is full
-            pass
-        self.storage.add_to_tail(item)
-        self.current += 1 # current must increase, but should be able to loop back to zero if it reaches capacity.
+            self.storage.remove_from_head()
+            self.storage.add_to_head(item)
+            # self.current += 1 % self.capacity # current must increase, but should be able to loop back to zero if it reaches capacity. Modulo.
+        else:
+            self.storage.add_to_tail(item)
 
 
     def get(self):
@@ -24,10 +25,11 @@ class RingBuffer:
         list_buffer_contents = []
 
         # returns all of the elements in the buffer in a list in their given order (OLDEST VALUE FIRST? if so, not necessarily head or tail)
-        # do not return None values in the list even if they are present
-        for entry in range(self.storage.length):
-           list_buffer_contents.append(entry)
-
+        # do not return None values in the list even if they are present:
+        while self.current is not None:
+            list_buffer_contents.append(self.current.value)
+            self.current = self.current.next
+        
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
